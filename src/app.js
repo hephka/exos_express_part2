fe9a226d142e2c14ad6e9ca51c3f294542b969a6
hephka <franckthevenin95@gmail.com>
2020-10-02 13:57:07 +0200

    corr

--- src/app.js
+++ src/app.js
@@ -1,36 +1,55 @@
 import express from 'express'
 import chalk from 'chalk'
+import { calc } from './calc.js'
 
 const PORT = 7777
 const IP = '192.168.1.14'
-const title = `
-<header>
-<h1 
-    style="text-align: 
-    center; font-size: 3rem; 
-    margin-top: 2rem; 
-    color: #686868"
->
-Exercices <em>express</em> partie 2
-</h1>
-</header>
-`
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send(title)
+const sendMessage = (req, res) => {
+    res.send(req.message)
+}
+
+const wrappWithHtml = (req, res, next) => {
+    const html = `
+    <!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercices express 2</title>
+  </head>
+
+  <body>
+    <p>${req.message}</p>
+  </body>
+</html>
+`
+    req.message = html
+    next()
+}
+
+app.get('/', (req, res, next) => {
+    req.message = `Exercices express partie 2`
+    next()
 })
 
-app.get('/get_current_time', (req, res) => {
-    const date = new Date()
-    res.send(title+`${date.toUTCString()}`)
+app.get('/get_current_time', (req, res, next) => {
+    const date = new Date().toUTCString()
+    req.message = date
+    next()
 })
 
-app.get('/how_pass_data', (req, res) => {
-    res.send(title+`Pour passer des données handler/middleware à un express nous leur ajoutons un paramètre <i>next</i> que nous appelons avec <i>next()</i>`)
+app.get('/how_pass_data', (req, res, next) => {
+    const msg = `On peut passer des données entre les middleware grâce aux objs req, res`
+    req.message = msg
+    next()
 })
- 
+
+app.use('/calc', calc)
+app.use(wrappWithHtml)
+app.use(sendMessage)
 app.listen(PORT, IP, () => {
-    console.log(`Exercice `+chalk.bgBlue(`express`)+` listening at `+chalk.green(`http://${IP}:${PORT}`))
+    console.log(`listening at ` + chalk.green(`${IP}:${PORT}`))
 })
